a997eecb21309eef218d42725a9e5efb524bc9ad
369cmgsosu-debug <369cmgsosu@gmail.com>
2026-06-11 13:59:57 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-const express = require("express");
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public")); // ←これ追加
-
 let list = [];
 
-// 画面表示
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-// データ追加
 app.post("/add", (req, res) => {
-  list.push(req.body.text);
+  const data = {
+    date: req.body.date,
+    person: req.body.person,
+    place: req.body.place,
+    status: req.body.status,
+    comment: req.body.comment
+  };
+
+  list.push(data);
+
   res.redirect("/");
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
