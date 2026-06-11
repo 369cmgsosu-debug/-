f3eddffd7f0576efb8e50dca349b54fce1e4be3c
369cmgsosu-debug <369cmgsosu@gmail.com>
2026-06-11 13:38:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(`
+    <h1>パトロール</h1>
+    <p>サイト完成！</p>
+  `);
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running");
-});
+app.listen(PORT);
